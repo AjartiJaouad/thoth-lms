a1f7a42a5182b3b22a4ddf78c02d52744de3f053
ajartijaouad <ajartijaouad14@gmail.com>
2026-01-15 11:11:48 +0100

    stup database config an dpdo concxion

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,7 +1,16 @@
 <?php
-class HomeController extends Controller{
-    public function index(){
-        $data = ['title'=>'BienVenue sur Thoth '];
-        $this->view('home',$data);
+require_once __DIR__ . '/../core/Controller.php';
+
+class HomeController
+{
+
+    private $dcontr;
+
+    public function index()
+    {
+        $this->dcontr = new Controller();
+        $data = ['title' => 'Bienvenue sur Thoth'];
+        $this->dcontr->view('home', $data);
     }
 }
+?>
